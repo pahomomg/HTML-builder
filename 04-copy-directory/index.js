064ba1b3fdca0e8b2a294a9ca9843dbed1155524
pahomomg <106546045+pahomomg@users.noreply.github.com>
2022-11-07 21:33:37 +0300

    Add files via upload

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const pathToData = path.join(__dirname, "files");
+const pathToCopyData = path.join(__dirname, "files-copy");
 
+fs.readdir(pathToCopyData, { withFileTypes: true }, (err, data) => {
+  if (err) {
+    fs.mkdir(
+      "./04-copy-directory/files-copy",
+      { recursive: true },
+      function (err) {
+        if (err) throw err;
+      }
+    );
+  } else {
+    data.forEach((file) => {
+      if (file.isFile()) {
+        let pathToCopyFile = path.join(pathToCopyData, file.name);
+        fs.unlink(pathToCopyFile, (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
+});
+
+fs.readdir(
+  pathToData,
+  {
+    withFileTypes: true,
+  },
+  (_, data) => {
+    data.forEach((file) => {
+      let pathToFile = path.join(pathToData, file.name);
+      let pathToCopyFile = path.join(pathToCopyData, file.name);
+      if (file.isFile()) {
+        fs.copyFile(pathToFile, pathToCopyFile, (error) => {
+          if (error) throw error;
+        });
+      }
+    });
+  }
+);
