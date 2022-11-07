7e4d98891c8dea5873ac27ec4bfe55fe8789854a
pahomomg <106546045+pahomomg@users.noreply.github.com>
2022-11-07 21:31:23 +0300

    Add files via upload

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const readStream = fs.createReadStream("./01-read-file/text.txt");
+/*readStream.on("data", (chunk) => {
+  console.log(chunk.toString());
+});*/
+readStream.pipe(process.stdout);
